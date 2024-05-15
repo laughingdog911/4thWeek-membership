0fb47e04033123062c30469b1e8243c992193474
laughingdog911 <estherghkd@gmail.com>
2024-05-15 16:07:37 +0900

    Added vector images at each dialogs. edited if-elseif-else. edited empty field checking algorithm. edited email checking algorithm.

--- app/src/main/java/com/hj/myapplication/MainActivity.kt
+++ app/src/main/java/com/hj/myapplication/MainActivity.kt
@@ -2,18 +2,21 @@ package com.hj.myapplication
 
 import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
+
 import com.hj.myapplication.databinding.ActivityMainBinding
+
+private fun AlertDialog.Builder.setItems(arrayOf: MutableList<String>) {
+}
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -26,20 +29,20 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+
+
+
     fun onClick(v: View) {
         lateinit var birthday: String
-        val phoneNumber = binding.phoneno.text
-        val memberType = binding.membertype.text
-        val email = binding.email.text
-        val name = binding.name.text
-        val infoList = listOf(email, name, birthday, phoneNumber, memberType)
-
-        val at: Char = '@'
-        var tag = email[0]
-        val l: Int = email.length
-
-        val EULAAgreed = binding.EULAAgree.isChecked
-        val InfoAgreed = binding.informaticsAgree.isChecked
+        val phoneNumber = binding.phoneno.text.toString()
+        val email = binding.email.text.toString()
+        val name = binding.name.text.toString()
+        val getPW = binding.pw.text.toString()
+        val getCheckPW = binding.checkpw.text.toString()
+        val infoList: MutableList<String> =
+            mutableListOf(email, name, birthday, phoneNumber, getPW, getCheckPW)
+        val eulaAgreed = binding.EULAAgree.isChecked
+        val infoAgreed = binding.informaticsAgree.isChecked
 
         when (v) {
             binding.choose -> {
@@ -50,78 +53,66 @@ class MainActivity : AppCompatActivity() {
                 val eventHandler = DialogInterface.OnClickListener { p0, p1 ->
                     if (p1 == DialogInterface.BUTTON_NEGATIVE)
                         null else if (p1 == DialogInterface.BUTTON_POSITIVE) {
-                            birthday = binding.bdaydp.text.toString()
+                        birthday = binding.bdaydp.text.toString()
                     }
                 }
             }
+
             binding.confirmButton -> {
 
-                for (i in infoList) {
-                    if (i == null) AlertDialog.Builder(this).run {
+                for (i in infoList.indices) {
+                    if (infoList[i].isEmpty()) {
+                        infoList[i] = null.toString()
+                    }
+                }
+                if (infoList.any { false }) {                                  //1. 공백필드
+                    AlertDialog.Builder(this).run{
+                        setIcon(android.R.drawable.ic_dialog_alert)
                         setTitle("공백 필드")
                         setMessage("모든 필드를 채워주세요.")
                         setPositiveButton("확인", null)
                         show()
-                    }
                 }
-
-                for (c in email) {
-                    if (tag != at) {
-                        tag = email[+1]
-                        when (tag == email[l - 1]) {
-                            true -> AlertDialog.Builder(this).run {
-                                setTitle("잘못된 이메일")
-                                setMessage("잘못된 형식의 이메일입니다.")
-                                setPositiveButton("확인", null)
-                                show()
-                            }
-
-                            false -> null
-
-                        }
+                } else if (!email.contains("@")) {                        //2. 잘못된 이메일 형식
+                    AlertDialog.Builder(this).run {
+                        setIcon(android.R.drawable.ic_dialog_alert)
+                        setTitle("잘못된 이메일")
+                        setMessage("잘못된 형식의 이메일입니다.")
+                        setPositiveButton("확인", null)
+                        show()
                     }
-
-                }
-
-                val getPW = binding.pw.text
-                if (getPW.length != 7){    //비밀번호의 길이가 8이 아니라면,
-                    AlertDialog.Builder(this). run {
+                } else if (getPW.length <= 7) {                                //3. 안전하지 않은 비밀번호
+                    AlertDialog.Builder(this).run {
+                        setIcon(android.R.drawable.ic_dialog_alert)
                         setTitle("안전하지 않은 비밀번호")
                         setMessage("보안을 위해 8자리 이상의 비밀번호를 설정해주세요.")
                         setPositiveButton("확인", null)
-                        show()           //Dialog 띄우기.
+                        show()
                     }
-                }
-
-                val getCheckPW = binding.checkpw.text
-                if (getCheckPW != getPW) {
+                } else if (getCheckPW != getPW) {                              //4. 비밀번호 확인과 불일치
                     AlertDialog.Builder(this).run {
+                        setIcon(android.R.drawable.ic_dialog_alert)
                         setTitle("일치하지 않는 비밀번호")
                         setMessage("비밀번호와 비밀번호 확인이 일치하지 않습니다.")
                         setPositiveButton("확인", null)
                         show()
                     }
-                }
-
-
-                if (EULAAgreed && InfoAgreed) {
-                    null
-                } else {
+                } else if (!eulaAgreed && !infoAgreed) {                       //5. 계약서 미동의
                     AlertDialog.Builder(this).run {
+                        setIcon(android.R.drawable.ic_dialog_alert)
                         setTitle("동의 필요")
                         setMessage("최종 사용권 계약과 개인정보 수집 및 처리 방침을 읽고 동의해주세요.")
                         setPositiveButton("확인", null)
                     }
-                }
-            }
-
-            else {
-                AlertDialog.Builder(this).run {
-                    setTitle("회원가입")
-                    setMessage("다음 정보로 가입을 진행하시겠습니까?")
-                    setItems(infoList)
-                    setPositiveButton("예", null)
-                    setNegativeButton("아니오", null)
+                } else {                                                       //6. 마지막 회원가입 확인
+                    AlertDialog.Builder(this).run {
+                        setIcon(android.R.drawable.ic_dialog_info)
+                        setTitle("회원가입")
+                        setMessage("다음 정보로 가입을 진행하시겠습니까?\n")
+                        setItems(infoList)
+                        setPositiveButton("예", null)  //setProgressBarVisibility()
+                        setNegativeButton("아니오", null)
+                    }
                 }
             }
         }
